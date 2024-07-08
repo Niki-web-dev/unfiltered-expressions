f1fba57696b3f9580a0bd207e864c73ff7bda5c6
Your Name <mykolai.b@hallwil.com>
2024-07-08 08:36:07 +0300

    Update Home page to last version

--- js/index.js
+++ js/index.js
@@ -23,7 +23,7 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 });
 
-//gsap H1 text animation
+//gsap H1 Hero text animation
 document.addEventListener('DOMContentLoaded', function () {
   window.addEventListener('load', function () {
     const words = document.querySelectorAll('[data-words-slide-up]');
@@ -48,9 +48,6 @@ document.addEventListener('DOMContentLoaded', function () {
       });
     }, 1000);
 
-    // infinity navbar scroll text function
-    // infinity navbar scroll text function
-    // infinity navbar scroll text function
     // infinity navbar scroll text function
     let counter = 0;
 
@@ -85,7 +82,6 @@ document.addEventListener('DOMContentLoaded', function () {
         repeat: -1,
       });
 
-      // Додаємо обробники подій для наведенні і відведенні
       item.parentElement.addEventListener('mouseenter', () => {
         iconAnimation.play();
       });
@@ -105,20 +101,104 @@ document.addEventListener('DOMContentLoaded', function () {
   gsap.registerPlugin(ScrollTrigger, InertiaPlugin);
 
   const images = document.querySelectorAll('.hero__img');
+  let scrollDistanse = 200;
+
+  if (window.innerWidth < 480) {
+    scrollDistanse = scrollDistanse * -1;
+  }
 
   images.forEach((img, i) => {
     gsap.to(img, {
-      y: i % 2 === 0 ? 200 : -200, // Вибір напрямку зміщення на основі індексу
+      y: i % 2 === 0 ? scrollDistanse : scrollDistanse * -1,
       inertia: {
-        resistance: 200, // Опір інерції
+        resistance: 200, // scroll resist
       },
       scrollTrigger: {
         trigger: '.hero',
-        start: 'top top', // Початок анімації
-        end: 'bottom top', // Кінець анімації
-        scrub: 1, // Плавність анімації з певним затриманням
-        inertia: true, // Додаємо інерцію
+        start: 'top top',
+        end: 'bottom top',
+        scrub: 1,
+        inertia: true,
       },
     });
   });
+
+  //work galley scroll while scroll page
+  const worksWrapper = document.querySelector('.works__wrapper');
+  const works = document.querySelector('.works');
+
+  const wrapperWidth = worksWrapper.scrollWidth;
+  const containerWidth = works.clientWidth;
+  const scrollDistance = wrapperWidth - containerWidth;
+
+  const scrollAmount = scrollDistance > 0 ? scrollDistance : 0;
+
+  gsap.to(worksWrapper, {
+    x: -scrollAmount,
+    ease: 'none',
+    scrollTrigger: {
+      trigger: works,
+      start: 'top top',
+      end: `+=${works.clientHeight * 0.5}`,
+      scrub: true,
+    },
+  });
+
+  //change color for navbar if scroll page
+  const navbar = document.querySelector('.nav__container');
+  let isNavbarDark = false;
+
+  function updateNavbarStyles() {
+    if (window.innerWidth < 480) {
+      const shouldMakeNavbarDark = window.scrollY > 220;
+
+      if (shouldMakeNavbarDark) {
+        navbar.classList.add('active');
+      } else {
+        navbar.classList.remove('active');
+      }
+
+      isNavbarDark = shouldMakeNavbarDark;
+    }
+  }
+
+  window.addEventListener('load', updateNavbarStyles);
+  window.addEventListener('scroll', updateNavbarStyles);
+  window.addEventListener('resize', updateNavbarStyles);
+  //mobile slider
+
+  function createSlider() {
+    let swiper;
+
+    if (window.innerWidth < 480) {
+      var link = document.createElement('link');
+      link.classList.add('swiper-css');
+      link.rel = 'stylesheet';
+      link.href = 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css';
+      document.head.appendChild(link);
+
+      if (!swiper) {
+        swiper = new Swiper('.swiper', {
+          slidesPerView: 1,
+          spaceBetween: 10,
+          pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+          },
+        });
+      }
+    } else {
+      if (document.querySelector('.swiper-css')) {
+        document.querySelector('.swiper-css').remove();
+      }
+
+      if (swiper) {
+        swiper.destroy();
+        swiper = undefined;
+      }
+    }
+  }
+
+  createSlider();
+  window.addEventListener('resize', createSlider);
 });
