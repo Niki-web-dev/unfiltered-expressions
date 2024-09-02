bacea91fa186fe320e6eb672ffa3c5d770d3fb7c
Your Name <mykolai.b@hallwil.com>
2024-09-02 06:17:19 +0300

    BIG SCROLLABLE update 2

--- js/product.js
+++ js/product.js
@@ -1,6 +1,7 @@
 //work galley scroll while scroll page
 document.addEventListener('DOMContentLoaded', function () {
   window.scrollTo(0, 0);
+  gsap.registerPlugin(ScrollTrigger, InertiaPlugin);
 
   const worksWrapper = document.querySelector('.works__wrapper');
   const works = document.querySelector('.detail-works');
@@ -21,8 +22,8 @@ document.addEventListener('DOMContentLoaded', function () {
     ease: 'none',
     scrollTrigger: {
       trigger: works,
-      start: '60%',
-      end: `+=${works.clientHeight * 0.9}`,
+      start: 'top',
+      end: `+=${works.clientHeight * 0.5}`,
       scrub: 2.5,
     },
   });
@@ -76,5 +77,8 @@ document.addEventListener('DOMContentLoaded', function () {
   container.addEventListener('wheel', handleScroll);
 
   // Update maxScroll value when the window is resized
-  window.addEventListener('resize', updateMaxScroll);
+  window.addEventListener('resize', () => {
+    updateMaxScroll();
+    ScrollTrigger.refresh();
+  });
 });
